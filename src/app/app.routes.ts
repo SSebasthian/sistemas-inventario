b18ac7d083c1b7ecb23dc88a6ac5fc3ffbf91140
SSebasthian <ssebasthian.perez@gmail.com>
2025-01-17 14:10:01 -0500

    Se Enruta Pagina de (ACCESO)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AccesoComponent } from './pagina/autenticacion/acceso/acceso.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '',
+        component: AccesoComponent
+    }
+];
