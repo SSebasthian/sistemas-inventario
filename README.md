623241b59cc3fd2a3db47b072771126a7c9e1c3b
SSebasthian <ssebasthian.perez@gmail.com>
2025-01-17 13:47:46 -0500

    Se Agregan componentes de Firebase

--- README.md
+++ README.md
@@ -5,6 +5,18 @@
 `ng add @angular/material@18`<br>
 
 
+## Necesario Firebase
+<b>Se Crea Proyecto en Firebase con Authentication y Firestore Database</b>
+`npm install firebase`<br>
+`ng add @angular/fire`<br>
+`npm install -g firebase-tools`<br>
+
+(seleccionamos Authentication / firestore )<br>
+(Seguimos los pasos para autenticarnos con la cuenta de Firebase)<br>
+(Seleccionamos el correo con el que se registró Firebase)<br>
+(Seleccionamos el proyecto creado con Firebase (sistemas-inventario))<br>
+
+`firebase login`<br>
 
 ## Creación de Proyecto Angular
 `ng new Sistemas-Inventario` (Se crea con CSS)<br>
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"sistemas-inventario","appId":"1:694019300740:web:5b9977bd6c259378b991d1","storageBucket":"sistemas-inventario.firebasestorage.app","apiKey":"AIzaSyBdIxaR0NHy3UEAlSiFLtqQvGFn_xQ_rmc","authDomain":"sistemas-inventario.firebaseapp.com","messagingSenderId":"694019300740"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
