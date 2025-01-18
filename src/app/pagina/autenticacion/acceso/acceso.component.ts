84f9649db5a5c2ff968c691b4da106d751b6cfbc
SSebasthian <ssebasthian.perez@gmail.com>
2025-01-18 07:29:48 -0500

    Se Diseña Autenticacion (ACCESO)

--- src/app/pagina/autenticacion/acceso/acceso.component.ts
+++ src/app/pagina/autenticacion/acceso/acceso.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-acceso',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './acceso.component.html',
   styleUrl: './acceso.component.css'
 })
