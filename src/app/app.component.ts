ba5510d42dbfa9836533be34007be8cf0b3d4c5f
SSebasthian <ssebasthian.perez@gmail.com>
2025-01-23 12:05:22 -0500

    Se Corrige Problema (Se Mostraba Menu En Pagina Acceso) - Solo Se Muestra Al Iniciar Sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './pagina/menu/menu.component';
 import { MatIconModule } from '@angular/material/icon';
+import { AutenticadorService } from './arquitectura/servicio/autenticador.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -9,15 +14,26 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     RouterOutlet,
     MenuComponent,
-    MatIconModule
+    MatIconModule,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sistemas-inventario';
-
   menuMinimizado = false; // Estado inicial del menú
+  isSesionIniciada$!: Observable<boolean>; // Observable para verificar la sesión
+  
+
+  constructor(private autenticadorServicio: AutenticadorService) {}
+
+  ngOnInit() {
+    // Transformar el estado de autenticación en un booleano
+    this.isSesionIniciada$ = this.autenticadorServicio.estadoIngresoSistema$.pipe(
+      map((usuario) => !!usuario) // `true` si el usuario existe, `false` de lo contrario
+    );
+  }
 
   menuMinMax() {
     this.menuMinimizado = !this.menuMinimizado; // Cambia el estado al hacer clic
