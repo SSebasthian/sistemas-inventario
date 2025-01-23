5cfa508bb53c9dc7694147541639fb91d9565883
SSebasthian <ssebasthian.perez@gmail.com>
2025-01-23 09:20:54 -0500

    Se Divide Pantalla Menu Vertical y PaginasDinamicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './pagina/menu/menu.component';
-
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MenuComponent
+    MenuComponent,
+    MatIconModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sistemas-inventario';
+
+  menuMinimizado = false; // Estado inicial del menú
+
+  menuMinMax() {
+    this.menuMinimizado = !this.menuMinimizado; // Cambia el estado al hacer clic
+  }
+
 }
