b00acc5fc49cfce66acb715da2fffb257259b525
SSebasthian <ssebasthian.perez@gmail.com>
2025-02-03 10:31:53 -0500

    Se Diseña Pagina  Registro (Usuario)

--- src/app/pagina/registro/reg-usuario/reg-usuario.component.ts
+++ src/app/pagina/registro/reg-usuario/reg-usuario.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-reg-usuario',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    
+  ],
   templateUrl: './reg-usuario.component.html',
   styleUrl: './reg-usuario.component.css'
 })
