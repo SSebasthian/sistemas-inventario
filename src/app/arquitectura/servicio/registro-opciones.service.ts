112c63be7b13ddb7114452ee3b348e9ff4098ca6
SSebasthian <ssebasthian.perez@gmail.com>
2025-02-06 12:37:27 -0500

    Se Registra Usuario a Firebase (SIN AREA - INF-CRITICA)

--- src/app/arquitectura/servicio/registro-opciones.service.ts
+++ src/app/arquitectura/servicio/registro-opciones.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Firestore, addDoc, collection, doc, getDoc, setDoc} from '@angular/fire/firestore';
+import Usuario from '../interface/usuario.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistroOpcionesService {
 
-  constructor() { }
+  //se injecta firestore
+  constructor(private firestore: Firestore) {  }
+
+  async agregarUsuario(usuario: Usuario) {
+    // Primero verificamos si ya existe un documento con la cédula
+    const usuarioReferencia = doc(this.firestore, 'usuario', usuario.cedula.toString());
+    const revisaCedula = await getDoc(usuarioReferencia);
+
+    if (revisaCedula.exists()) {
+      console.log("El usuario ya existe");
+      alert("El usuario ya existe");
+      return null; // O puedes devolver algún mensaje de error si el usuario ya está registrado
+    } else {
+      // Si no existe, guardamos el nuevo usuario
+      alert("Agregado con Exito");
+      return setDoc(usuarioReferencia, usuario);
+    }
+  }
+
+  nuevaArea(usuario: Usuario){
+    //se guarda coleccion del formulario en base de datos
+    const areaReferencia = collection(this.firestore, 'area');
+    return addDoc(areaReferencia, usuario);
+  }
 }
--- src/app/pagina/registro/reg-usuario/reg-usuario.component.ts
+++ src/app/pagina/registro/reg-usuario/reg-usuario.component.ts
@@ -1,17 +1,42 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RegistroOpcionesService } from '../../../arquitectura/servicio/registro-opciones.service';
 
 @Component({
   selector: 'app-reg-usuario',
   standalone: true,
   imports: [
     MatIconModule,
-    
+    ReactiveFormsModule
   ],
   templateUrl: './reg-usuario.component.html',
   styleUrl: './reg-usuario.component.css'
 })
 export class RegUsuarioComponent {
+  formularioUsuario: FormGroup;
 
+
+  constructor(private registroOpcionesService: RegistroOpcionesService){
+    this.formularioUsuario = new FormGroup({
+      cedula: new FormControl(),
+      apellido: new FormControl(),
+      nombre: new FormControl(),
+      area: new FormControl(),
+      cargo: new FormControl(),
+      infCritica: new FormControl(),
+      correo: new FormControl(),
+      claveCorreo: new FormControl(),
+      extenciontel: new FormControl(), 
+      fechaIngreso: new FormControl(),
+      estado: new FormControl(true) 
+    });
+  }
+
+  async registrarUsuario() {
+    console.log(this.formularioUsuario.value)
+    const usuarioGuardado = await this.registroOpcionesService.agregarUsuario(this.formularioUsuario.value)
+    console.log(usuarioGuardado);
+  }
+  
 }
