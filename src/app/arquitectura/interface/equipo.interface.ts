5d5e35fe82b8346aa3f30324073d1d7e134801b7
SSebasthian <ssebasthian.perez@gmail.com>
2025-02-15 06:06:26 -0500

    Se Crea Interface EQUIPO

---
+++ src/app/arquitectura/interface/equipo.interface.ts
@@ -0,0 +1,20 @@
+export default interface equipo{
+    serial: string;
+    imgEquipo: string;
+    tipoEquipo: string;
+    sistemaOperativo: string;
+    versionSistemaOperativo: String;
+    bits: number;
+    marca: string;
+    modelo: string;
+    procesador: string;
+    ram: string;
+    tipoRam: string;
+    disco: string;
+    tipoDisco: string;
+    plaqueta: string;
+    descripcion: string;
+    fechaCompra: Date;
+    ordenCompra: string;
+    estado: string;
+}
