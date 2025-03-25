63437793f6b8227831762ff9f00a976e0689ec53
SSebasthian <ssebasthian.perez@gmail.com>
2025-03-25 12:15:40 -0500

    Se Crean Opciones en Formularios Segun Tipo Equipo - Marca - Sistema Operativo (REGISTRO EQUIPO)

--- src/app/pagina/registro/reg-equipo/reg-equipo.component.ts
+++ src/app/pagina/registro/reg-equipo/reg-equipo.component.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms'; 
 
 
 @Component({
@@ -7,10 +9,26 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   imports: [
     MatIconModule,
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './reg-equipo.component.html',
   styleUrl: './reg-equipo.component.css'
 })
 export class RegEquipoComponent {
+  seleccionTipoEquipo: string = '';
+  seleccionMarca: string = '';
+  seleccionModelo: string = '';
+  seleccionSistemaOperativo : string = '';
+  seleccionSistemaOperativoVersion: string = '';
+  
+  constructor (){ }
 
+ 
+
+  
+
+
+  
 }
+
