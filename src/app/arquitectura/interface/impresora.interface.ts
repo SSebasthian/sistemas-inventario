94439085b1cc6e0b4e266a353ca1e4e4ffd418b3
SSebasthian <ssebasthian.perez@gmail.com>
2025-04-30 12:27:41 -0500

    Se Crea Interface IMPRESORA

---
+++ src/app/arquitectura/interface/impresora.interface.ts
@@ -0,0 +1,21 @@
+export default interface impresora{
+    serial: string;
+    imgImpresora: string;
+    tipoImpresora: string;
+    marca: string;
+    modelo: string;
+    tinta: string;
+    conectividad: string;
+    plaqueta: string;
+    descripcion: string;
+    fechaCompra: Date;
+    ordenCompra: string;
+    estado: string;
+}
+ 
+
+
+
+
+
+    
